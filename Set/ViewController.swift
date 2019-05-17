4dee1cdb196905b241f254c2ff8d7a0054f20714
Bronson Knowles <bronsonknowles@Bronsons-Mac-mini.local>
2019-05-16 22:24:23 -0400

    First commit

---
+++ Set/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Set
+//
+//  Created by Bronson Knowles on 5/16/19.
+//  Copyright © 2019 Bronson Knowles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBOutlet private weak var scoreLabel: UILabel!
+    @IBOutlet private weak var newGameButton: UIButton!
+    
+    @IBAction private func touchNewGameButton(_ sender: UIButton) {
+    }
+}
+
