2d931f1f374e0c4b56342fc74006d845c0c7dd75
Bronson Knowles <bronsonknowles@Bronsons-Mac-mini.local>
2019-05-17 10:41:20 -0400

    Working on UI autolayout

--- Set/ViewController.swift
+++ Set/ViewController.swift
@@ -17,8 +17,26 @@ class ViewController: UIViewController {
 
     @IBOutlet private weak var scoreLabel: UILabel!
     @IBOutlet private weak var newGameButton: UIButton!
+    @IBOutlet private var cardButtons: [UIButton]!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        sender.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        let attributes: [NSAttributedString.Key: Any] = [
+            .strokeWidth: 5.0,
+            .strokeColor: #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        ]
+        let cardAttributedText = NSAttributedString(string: "▲▲▲", attributes: attributes)
+        sender.setAttributedTitle(cardAttributedText, for: UIControl.State.normal)
+    }
     
     @IBAction private func touchNewGameButton(_ sender: UIButton) {
     }
+    
+    private var numberOfDrawnCards = 12
+    
+    //lazy private var game = Set()
+    
+    
+    
 }
 
